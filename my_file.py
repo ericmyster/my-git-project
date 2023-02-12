2f8d664e87c3067b0f84e91ba54f6decc2b58640
ericmyster <codeerich@gmail.com>
2023-02-11 21:08:26 -0700

    fixed Indention

--- my_file.py
+++ my_file.py
@@ -4,7 +4,7 @@ def hi_there():
     print('Hi There!')
     
     " Another Code Change"
-        " Another Code Change"
+    " Another Code Change"
 
 def hi_there():
     print('Updated python for merge conflict')
